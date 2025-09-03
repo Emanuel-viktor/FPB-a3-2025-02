ebb076948e2fe5cfbf61aa5a1290b561d3b38727
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2025-09-03 18:43:12 -0300

    Project created

---
+++ Backend/src/main/java/com/FPBa3/FPB_A3_2025_2/FpbA320252Application.java
@@ -0,0 +1,13 @@
+package com.FPBa3.FPB_A3_2025_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FpbA320252Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FpbA320252Application.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/FPBa3/FPB_A3_2025_2/FpbA320252ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FPBa3.FPB_A3_2025_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpbA320252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
